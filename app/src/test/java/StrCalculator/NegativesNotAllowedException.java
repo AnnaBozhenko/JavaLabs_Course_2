96f656c7ef50206e0d583611dbabda443b768d28
Anna Bozhenko <ms.anna.bozhenko.03@gmail.com>
2021-11-17 21:32:45 +0200

    Created ecxeption for negative numbers.

---
+++ app/src/test/java/StrCalculator/NegativesNotAllowedException.java
@@ -0,0 +1,7 @@
+package StrCalculator;
+
+public class NegativesNotAllowedException extends Exception {
+    public NegativesNotAllowedException (String message) {
+        super(message);
+    }
+}
