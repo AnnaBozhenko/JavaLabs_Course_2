3edbcd0f036bd4f2d425ee62fe8807b0ea78a737
Anna Bozhenko <ms.anna.bozhenko.03@gmail.com>
2021-11-17 22:06:46 +0200

    Added feature of showing all passed negative numbers when ecxeption is caught.

--- app/src/test/java/StrCalculator/StringCalculatorTest.java
+++ app/src/test/java/StrCalculator/StringCalculatorTest.java
@@ -1,11 +1,16 @@
 package  StrCalculator;
 
+import java.util.ArrayList;
+
 public class StringCalculatorTest {
 
     public int add(String numbersStr) throws NegativesNotAllowedException {
         int sum = 0;
+//        initiate a container for possible digits of a unique passed number
         StringBuilder num = new StringBuilder(numbersStr.length());
         char[] numbersArr = numbersStr.toCharArray();
+//        initiate a container for possible passed negative numbers
+        ArrayList<Integer> negativeNumbers = new ArrayList<>();
         if (numbersArr[0] == '/' && numbersArr[1] == '/' && numbersArr[3] == '\n') {
             char delimiter = numbersArr[2];
             for (int i = 4; i < numbersArr.length; i++) {
@@ -16,22 +21,24 @@ public class StringCalculatorTest {
                             sum = 0;
                             break;
                         } else if (Integer.parseInt(num.toString()) < 0) {
-                            throw new NegativesNotAllowedException(String.format("negatives not allowed, passed number: %d", Integer.parseInt(num.toString())));
+                            negativeNumbers.add(Integer.parseInt(num.toString()));
                         }
                         sum += Integer.parseInt(num.toString());
+                        if (negativeNumbers.size() != 0) throw new NegativesNotAllowedException(String.format("negatives not allowed, passed numbers: %s", negativeNumbers));
                     }
                 } else if (numbersArr[i] == delimiter || numbersArr[i] == '\n' && !num.toString().equals("")) {
                     if (i + 1 == numbersArr.length || numbersArr[i + 1] == delimiter || numbersArr[i + 1] == '\n') {
                         sum = 0;
                         break;
-                    } else {
+                    }
+                    else {
                         if (num.toString().equals("-")) {
                             sum = 0;
                             break;
                         } else if (Integer.parseInt(num.toString()) < 0) {
-                            throw new NegativesNotAllowedException(String.format("negatives not allowed, passed number: %d", Integer.parseInt(num.toString())));
+                            negativeNumbers.add(Integer.parseInt(num.toString()));
                         }
-                        sum += Integer.parseInt(num.toString());
+                        else sum += Integer.parseInt(num.toString());
                         num.delete(0, num.capacity() - 1);
                     }
                 } else {
