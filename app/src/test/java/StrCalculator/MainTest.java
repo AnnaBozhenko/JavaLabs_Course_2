92e0b1fedd4288c8dcd23b2c1923cd5428a39689
Anna Bozhenko <ms.anna.bozhenko.03@gmail.com>
2021-12-08 03:55:11 +0200

    Added new tests for multiple-characters delimiter.

--- app/src/test/java/StrCalculator/MainTest.java
+++ app/src/test/java/StrCalculator/MainTest.java
@@ -23,19 +23,25 @@ public class MainTest {
 
     private static Stream<Arguments> strCalculatorTesterAnySizeDelimiter() {
         return Stream.of(
+                Arguments.of("//[****]\n1-8", 0),
+                Arguments.of("//[****", 0),
+                Arguments.of("//[]\n1", 1),
                 Arguments.of("//[****]\n1", 1),
                 Arguments.of("//[^^^^^^^^^]\n1^^^^^^^^^2", 3),
                 Arguments.of("//[$]\n4$5$6", 15),
                 Arguments.of("//[!!]\n59!!2!!10005", 61),
-                Arguments.of("//[.]\n", 0)
-        );
+                Arguments.of("//[.]\n", 0),
+                Arguments.of("//[(^_^)]\n3(^_^)8(^_^)12", 23),
+                Arguments.of("//[@!@]\n1@!@3@!", 0)
+                );
     }
+
 
     @ParameterizedTest(name = "{index} => string = {0}, result = {1}")
     @MethodSource("strCalculatorTesterAnySizeDelimiter")
     void testStrCalculatorCalculatorTesterAnySizeDelimiter(String strNumbers, int expected) {
         try {
-            assertEquals(expected, testable.add(strNumbers), 0f);
+            assertThat(testable.add(strNumbers), is(expected));
         } catch (NegativesNotAllowedException e) {
             e.printStackTrace();
         }
@@ -89,35 +95,31 @@ public class MainTest {
 //    }
 
 
-    @Ignore
-    @Test
-    @DisplayName("Testing of how many times add() was called.")
-    public void testGetCalledCount() {
-        try {
-            int expectedTimesCalled = 5;
-            String testableStr = "//,\n1,2,3";
-
-            testable.add(testableStr);
-            testable.add(testableStr);
-            testable.add(testableStr);
-            testable.add(testableStr);
-            testable.add(testableStr);
-            assertThat(testable.GetCalledCount(), is(expectedTimesCalled));
-        } catch (NegativesNotAllowedException e) {
-            e.printStackTrace();
-        }
-    }
-
+//    @Test
+//    @DisplayName("Testing of how many times add() was called.")
+//    public void testGetCalledCount() {
+//        try {
+//            int expectedTimesCalled = 5;
+//            String testableStr = "//,\n1,2,3";
+//
+//            testable.add(testableStr);
+//            testable.add(testableStr);
+//            testable.add(testableStr);
+//            testable.add(testableStr);
+//            testable.add(testableStr);
+//            assertThat(testable.GetCalledCount(), is(expectedTimesCalled));
+//        } catch (NegativesNotAllowedException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
 
-
-
-    @Ignore
-    @Test
-    @DisplayName("Test to verify if exception for passed negative numbers was thrown.")
-    public void testException() {
-        assertThrows(NegativesNotAllowedException.class, () -> testable.add(strToTest));
-    }
+//    @Ignore
+//    @Test
+//    @DisplayName("Test to verify if exception for passed negative numbers was thrown.")
+//    public void testException() {
+//        assertThrows(NegativesNotAllowedException.class, () -> testable.add(strToTest));
+//    }
 }
 
 
